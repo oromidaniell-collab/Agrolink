96ea77a2466caba3cfb8a142e5258c22022a6f5f
oromidaniell-collab <oromidaniell.com>
2026-06-08 13:36:05 +0300

    Redis client errors

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,11 +1,11 @@
 const Redis = require('ioredis');
 require('dotenv').config();
 
-// Create Redis client without immediate connection attempt
-// ioredis will reconnect automatically
+// Create Redis client.
+// This client will try to connect, but we don't want Redis to crash the whole server.
 const redis = new Redis({
     host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || 6379,
+    port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
     password: process.env.REDIS_PASSWORD || null,
     username: process.env.REDIS_USER_NAME || 'default',
     retryStrategy: (times) => Math.min(times * 50, 2000),
